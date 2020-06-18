fe0681fc029c86d8b07275ae3c57d03feb6ea4c4
Zachary Hardin <zbhardin@gmail.com>
2020-06-18 17:09:06 -0500

    Refactor input into a variable.

--- albums-app/src/app/lookup-form/lookup-form.component.spec.ts
+++ albums-app/src/app/lookup-form/lookup-form.component.spec.ts
@@ -6,6 +6,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 describe('LookupFormComponent', () => {
   let component: LookupFormComponent;
   let fixture: ComponentFixture<LookupFormComponent>;
+  let albumInputElement: HTMLInputElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,6 +20,8 @@ describe('LookupFormComponent', () => {
     fixture = TestBed.createComponent(LookupFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    albumInputElement = fixture.nativeElement.querySelector('input#albumInput');
   });
 
   it('should create', () => {
@@ -27,18 +30,16 @@ describe('LookupFormComponent', () => {
 
   describe('AlbumInput', () => {
     it('should render an input element', () => {
-      expect(fixture.nativeElement.querySelector('input#albumInput')).toBeTruthy();
+      expect(albumInputElement).toBeTruthy();
     });
 
     it('should set placeholder text to "Enter a number 1 - 100"', () => {
       const placeholderText = 'Enter a number 1 - 100';
-      expect(fixture.nativeElement.querySelector('input#albumInput').placeholder).toEqual(placeholderText);
+      expect(albumInputElement.placeholder).toEqual(placeholderText);
     });
 
     it('should update the form when given a value', () => {
-      const input = fixture.nativeElement.querySelector('input#albumInput');
-      enterText(input, '1');
-
+      enterText(albumInputElement, '1');
       expect(component.form.get('albumId').value).toEqual('1');
     });
   });
