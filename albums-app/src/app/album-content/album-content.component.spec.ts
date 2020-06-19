6b3b073d0ec5b7789da9f654a37b20ffbe442314
Zachary Hardin <zbhardin@gmail.com>
2020-06-18 20:40:43 -0500

    Show Title and Image

--- albums-app/src/app/album-content/album-content.component.spec.ts
+++ albums-app/src/app/album-content/album-content.component.spec.ts
@@ -1,10 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlbumContentComponent } from './album-content.component';
+import {Album} from '../models/Album';
 
 describe('AlbumContentComponent', () => {
   let component: AlbumContentComponent;
   let fixture: ComponentFixture<AlbumContentComponent>;
+
+  const album = new Album();
+  album.id = 1;
+  album.title = 'FizzBuzz';
+  album.thumbnailUrl = 'https://giphy.com/gifs/cat-funny-lol-CqVNwrLt9KEDK';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,5 +27,26 @@ describe('AlbumContentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should populate the id', () => {
+    component.albums = [album];
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('#id').textContent).toEqual('1');
+  });
+
+  it('should populate the title', () => {
+    component.albums = [album];
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('#title').textContent).toEqual('FizzBuzz');
+  });
+
+  it('should populate the thumbnailUrl', () => {
+    component.albums = [album];
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('#thumbnailUrl').src).toEqual('https://giphy.com/gifs/cat-funny-lol-CqVNwrLt9KEDK');
   });
 });
