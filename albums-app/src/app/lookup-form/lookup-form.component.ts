a784a9e380e6bea13b95653496ab620a6ed6c08e
Zachary Hardin <zbhardin@gmail.com>
2020-06-20 09:27:40 -0500

    Clean up imports.

--- albums-app/src/app/lookup-form/lookup-form.component.ts
+++ albums-app/src/app/lookup-form/lookup-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {AlbumService} from '../services/album.service';
 import {take} from 'rxjs/operators';
