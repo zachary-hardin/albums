f61bbdb10ccc3dcc2841ab6a5fa42f053c46fabe
Zachary Hardin <zbhardin@gmail.com>
2020-06-20 11:47:05 -0500

    Test album service api content.

--- albums-app/src/app/services/album-service.spec.ts
+++ albums-app/src/app/services/album-service.spec.ts
@@ -1,6 +1,7 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AlbumService} from './album.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
 
 describe('AlbumService', () => {
   let albumService: AlbumService;
@@ -26,5 +27,23 @@ describe('AlbumService', () => {
     const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/photos?albumId=5');
     expect(req.request.method).toEqual('GET');
     req.flush({});
+  });
+
+  it('should return a list of albums', () => {
+    const http = TestBed.get(HttpClient);
+    spyOn(http, 'get').and.returnValue([{
+      albumId: 12,
+      id: 551,
+      title: 'Grumpy Cats',
+      url: 'https://giphy.com/gifs/cat-laugh-funny-q1MeAPDDMb43K',
+      thumbnailUrl: 'https://giphy.com/gifs/C9x8gX02SnMIoAClXa'
+    }]);
+
+    const albums = albumService.fetchAlbumBy('2');
+    expect(albums[0].albumId).toBe(12);
+    expect(albums[0].id).toBe(551);
+    expect(albums[0].title).toBe('Grumpy Cats');
+    expect(albums[0].url).toBe('https://giphy.com/gifs/cat-laugh-funny-q1MeAPDDMb43K');
+    expect(albums[0].thumbnailUrl).toBe('https://giphy.com/gifs/C9x8gX02SnMIoAClXa');
   });
 });
